3e6e7f100ed8ce6bee4272ef177601529d6efb5a
Logan Ladd Bielewicz <lbielewicz@cs.arizona.edu>
2019-10-07 22:24:50 -0700

    Added functionality for new tokens (still needs to be tested)

--- Scanner.java
+++ Scanner.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 
 class Token{
   enum TokenType{
-    NUM, PLUS, MINUS, MUL, DIV, LT, LTE, GT, GTE, OP, CP;
+    NUM, PLUS, MINUS, MUL, DIV, LT, LTE, GT, GTE, OP, CP, ID, ASSG, EQ, NEQ, INT, END, OB, CB, IF, WHILE, VOID;
   }
 
   TokenType tokenType;
@@ -43,10 +43,51 @@ public class Scanner{
         itr++;
         if (itr == stream.length())
           break;
-        tokChar = stream.charAt(itr);
       }
       
       ret = new Token(Token.TokenType.NUM, num);
+    }
+    
+    // Handling an INT token
+    else if (stream.length() > 2 && stream.substring(itr,itr+3).equals("int")) {
+      ret = new Token(Token.TokenType.INT, "int");
+      itr = itr + 3;
+    }
+
+    // Handling an IF token
+    else if (stream.length() > 1 && stream.substring(itr,itr+2).equals("if")) {
+      ret = new Token(Token.TokenType.IF, "if");
+      itr = itr + 2;
+    }
+
+    // Handling a WHILE token
+    else if (stream.length() > 4 && stream.substring(itr,itr+5).equals("while")) {
+      ret = new Token(Token.TokenType.WHILE, "while");
+      itr = itr + 5;
+    }
+
+    // Handling a VOID token
+    else if (stream.length() > 3 && stream.substring(itr,itr+4).equals("void")) {
+      ret = new Token(Token.TokenType.VOID, "void");
+      itr = itr + 4;
+    }
+    
+    // Handling an ID token
+    else if ((tokChar >= 'a' && tokChar <= 'z') || (tokChar >= 'A' && tokChar <= 'Z')) {
+      String id = "";
+      id += tokChar;
+      itr++;
+
+      while (itr < stream.length() && ((stream.charAt(itr) >= '0' && stream.charAt(itr) <= '9') ||
+             (stream.charAt(itr) >= 'a' && stream.charAt(itr) <= 'z') ||
+             (stream.charAt(itr) >= 'A' && stream.charAt(itr) <= 'Z'))) {
+        id += stream.charAt(itr);
+        itr++;
+        if (itr == stream.length())
+          break;
+      }
+
+      ret = new Token(Token.TokenType.ID, id);
     }
 
     // Handling a PLUS token
@@ -72,7 +113,24 @@ public class Scanner{
       ret = new Token(Token.TokenType.DIV, "/");
       itr++;
     }
+    
+    // Handling an NEQ token
+    else if (stream.length() > 1 && tokChar == '!' && stream.charAt(itr + 1) == '=') {
+      ret = new Token(Token.TokenType.NEQ, "!=");
+      itr = itr + 2;
+    }
 
+    // Handling EQ and ASSG tokens
+    else if (tokChar == '=') {
+      if (stream.length() > 1 && stream.charAt(itr + 1) == '=') {
+        itr++;
+        ret = new Token(Token.TokenType.EQ, "==");
+      }
+      else
+        ret = new Token(Token.TokenType.ASSG, "=");
+      itr++;
+    }
+    
     // Handling LTE and LT tokens
     else if (tokChar == '<') {
       if (stream.length() > 1 && stream.charAt(itr + 1) == '=') {
@@ -107,6 +165,24 @@ public class Scanner{
       itr++;
     }
     
+    // Handling an OB token
+    else if (tokChar == '{') {
+      ret = new Token(Token.TokenType.OB, "{");
+      itr++;
+    }
+
+    // Handling a CB token
+    else if (tokChar == '}') {
+      ret = new Token(Token.TokenType.CB, "}");
+      itr++;
+    }
+
+    // Handling an END token
+    else if (tokChar == ';') {
+      ret = new Token(Token.TokenType.END, ";");
+      itr++;
+    }
+
     // Handling invalid characters
     else {
       System.out.println("ERROR: Invalid character '" + tokChar + "'in " + stream + ". Exiting program.");
